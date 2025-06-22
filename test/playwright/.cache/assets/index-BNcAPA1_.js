4010922d88870142e25168313a3674b8e4648370
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2025-06-22 22:22:44 +0300

    Remove ignored Playwright result folders from repo

--- test/playwright/.cache/assets/index-BNcAPA1_.js
+++
@@ -1,7 +0,0 @@
-import { d as requireReactDom, g as getDefaultExportFromCjs } from './index-BNDLrXFc.js';
-
-var reactDomExports = requireReactDom();
-const ReactDOM = /*@__PURE__*/getDefaultExportFromCjs(reactDomExports);
-
-export { ReactDOM as R, reactDomExports as r };
-//# sourceMappingURL=index-BNcAPA1_.js.map
